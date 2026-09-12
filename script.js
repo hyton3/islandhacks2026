0f81620a25f002fdc7df5ce01932648c94510b82
ileeCode <ilee882288@gmail.com>
2026-09-12 13:08:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Variables
+/*Variables
 let score = 0;
 let streak = 0;
 let userGuess;
@@ -7,30 +7,38 @@ let trashBin
 let recycleBin;
 let compostBin;
 let buttonClicked;
-let itemList=[];
 let ifCorrect;
-let itemListAmount;
+*/
 
 
-function randomNumber(){
-    let number = Math.floor(Math.random() * itemListAmount) + 1;
-    console.log(number);
-}
-function randomItem(){
-    
-}
-function interface() {
-    function sort(){
+function itemGeneration(){
+    let itemList=[["Item 1",[1]], ["Item 2",[1]], ["Item 3",[1]], ["Item 4",[1]], ["Item 5",[1]], ["Item 6",[1]], ["Item 7",[1]], ["Item 8",[1]], ["Item 9",[1]], ["Item 10",[1]]];
+    let itemListAmount = itemList.length;
+    let itemNumber = itemListAmount - 1;
+    let number;
+    let itemVal;
 
-    }
-    /*
-    function guessItem(){
-        if (buttonClicked == Imagetype){
-
+    function generateItem(){
+        function randomNumber(){
+            itemNumber = Math.floor(Math.random() * itemListAmount) + 0;
+            return itemNumber;
         }
+    function randomItem(){
+        item = itemList[itemNumber];
+        return item;
     }
-        */
-    function updateScore() {
+    function getVal(){
+        itemVal = itemList[itemNumber][1];
+        return itemVal;
+    }
+    randomNumber();
+    randomItem();
+    getVal();
+}
+    generateItem();
+}
+itemGeneration();
 
-    }
+function sorting(itemVal){
+    console.log(itemVal);
 }

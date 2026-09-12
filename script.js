3d87a9ceb69e45fe68289d1ddf92a1a805f74866
ileeCode <ilee882288@gmail.com>
2026-09-12 13:38:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ let ifCorrect;
 
 
 function itemGeneration(){
-    let itemList=[["Item 1",[1]], ["Item 2",[1]], ["Item 3",[1]], ["Item 4",[1]], ["Item 5",[1]], ["Item 6",[1]], ["Item 7",[1]], ["Item 8",[1]], ["Item 9",[1]], ["Item 10",[1]]];
+    let itemList=[["Item 1",[1]], ["Item 2",[1]], ["Item 3",[1]], 
+    ["Item 4",[1]], ["Item 5",[1]], ["Item 6",[1]], ["Item 7",[1]], ["Item 8",[1]], ["Item 9",[1]], 
+    ["Item 10",[1]]
+    ];
     let itemListAmount = itemList.length;
     let itemNumber = itemListAmount - 1;
     let number;
@@ -39,6 +42,36 @@ function itemGeneration(){
 }
 itemGeneration();
 
-function sorting(itemVal){
-    console.log(itemVal);
+
+//if it doesnt work, take this var and put in every function
+let binType;
+let score=0;
+function sortingTrash(itemVal, score){
+    binType = trash;
+    if (binType == itemVal){
+        //Run the animation for correct answer
+        score=score+1;
+    }else{
+        //Run the animation for incorrect anwswer
+    }
+
+}
+function sortingRecycle(itemVal, score){
+    binType = recycle;
+    if (binType == itemVal){
+        //Run the animation for correct answer
+        score=score+1;
+    } else{
+        //Run the animation for incorrect anwswer
+    }
+}
+function sortingCompost(itemVal, score){
+    binType = compost;
+    if (binType == itemVal){
+        //Run the animation for correct answer
+        score=score+1;
+    } else{
+        //Run the animation for incorrect anwswer
+    }
+    
 }
